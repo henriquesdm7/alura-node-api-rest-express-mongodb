279f72eac13d4537ace8ca8ebe87e928266aad93
Henrique <49873860+henriquesdm7@users.noreply.github.com>
2022-08-11 11:36:17 -0300

    feat: express implementation

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-const http = require('http');
-const port = 3000;
+import app from './src/app.js'
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Node Course');
-});
-
-server.listen(port, () => console.log(`Server listening on http://localhost:${port}`))
+app.listen(port, () => console.log(`Server listening on http://localhost:${port}`))
---
+++ src/app.js
@@ -0,0 +1,28 @@
+import Express from "express";
+
+const app = Express();
+
+const livros = [
+    {
+        id: 1,
+        titulo: "Senhor dos Anéis"
+    },
+    {
+        id: 2,
+        titulo: "O Hobbit"
+    }
+]
+
+app.get('/', (req, res) => {
+    res
+        .status(200)
+        .send('Curso de Node')
+});
+
+app.get('/livros', (req, res) => {
+    res
+        .status(200)
+        .json(livros)
+});
+
+export default app;
