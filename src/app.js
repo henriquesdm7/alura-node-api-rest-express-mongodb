acdf2248e5832df5430cf1fb7d1fc07cee66bd16
Henrique <49873860+henriquesdm7@users.noreply.github.com>
2022-08-11 15:26:18 -0300

    feat: api CRUD implementation

--- src/app.js
+++ src/app.js
@@ -11,18 +11,52 @@ const livros = [
         id: 2,
         titulo: "O Hobbit"
     }
-]
+];
+
+app.use(Express.json());
 
 app.get('/', (req, res) => {
     res
         .status(200)
-        .send('Curso de Node')
+        .send('Curso de Node');
 });
 
 app.get('/livros', (req, res) => {
     res
         .status(200)
-        .json(livros)
+        .json(livros);
 });
+
+app.get('/livros/:id', (req, res) => {
+    let index = searchBook(req.params.id);
+    res.json(livros[index]);
+});
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+
+    res
+        .status(201)
+        .send('Livro cadastrado com sucesso');
+});
+
+app.put('/livros/:id', (req, res) => {
+    let { id } = req.params;
+    let index = searchBook(id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+});
+
+app.delete('/livros/:id', (req, res) => {
+    let { id } = req.params;
+    let index = searchBook(id);
+    livros.splice(index, 1);
+    res.send(`Livro ${id} removido com sucesso`)
+});
+
+
+function searchBook(id) {
+    return livros.findIndex(livro => livro.id == id)
+}
 
 export default app;
